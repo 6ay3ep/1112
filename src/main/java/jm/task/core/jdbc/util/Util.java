3d08186abc3a84d7f8a8225471142fd43e37942b
6ay3ep <ob_lom666@inbox.ru>
2025-04-30 14:17:01 +0500

    git commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -12,7 +12,6 @@ public class Util {
         Connection connection = null;
         try  {
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-
         } catch (SQLException e) {
             throw new RuntimeException("Ошибка подкючения");
         }
