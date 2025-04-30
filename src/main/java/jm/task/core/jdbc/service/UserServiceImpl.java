359ddfd400405fe43f64f62ae889647e592121d2
6ay3ep <ob_lom666@inbox.ru>
2025-04-30 14:04:01 +0500

    git commit

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -26,7 +26,8 @@ public class UserServiceImpl implements UserService {
     }
 
     public List<User> getAllUsers() {
-        return null;
+        return userDao.getAllUsers();
+
     }
 
     public void cleanUsersTable() {
